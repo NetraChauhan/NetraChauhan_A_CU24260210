7e02846d244174b165a791b8c61383cad5879788
NetraChauhan <cu24260210@coeruniversity.ac.in>
2026-09-13 18:27:29 +0530

    Keep Program 22 weather card near heading

--- lib/program_22.dart
+++ lib/program_22.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: const Text('Weather Information'), centerTitle: true),
         body: Padding(
           padding: const EdgeInsets.fromLTRB(24, 18, 24, 24),
-          child: Center(
+          child: Align(
+            alignment: Alignment.topCenter,
             child: Card(
               elevation: 3,
               child: Container(
